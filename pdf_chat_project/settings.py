4f6cb9a0eee4eb64fb5267d85b47f1ff2a117bbb
Dev Nirwal <devnirwal@Newtons-MacBook-Pro-3.local>
2026-03-13 11:05:49 +0530

    Fix: add CSRF_TRUSTED_ORIGINS for live domain

--- pdf_chat_project/settings.py
+++ pdf_chat_project/settings.py
@@ -36,6 +36,8 @@ DEBUG = env.bool('DEBUG', default=False)
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['*'])
 
+CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS', default=['http://simple-rag.nstsdc.org', 'https://simple-rag.nstsdc.org'])
+
 
 # Application definition
 
